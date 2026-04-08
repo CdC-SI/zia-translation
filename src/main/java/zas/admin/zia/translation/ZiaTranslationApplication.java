95212b2f07618d1c8e4b18e80cd27dab371f0628
Mvinciar <matt.vincia@gmail.com>
2026-04-08 15:59:50 +0200

    initialize zia-translation project with Spring Boot setup

---
+++ src/main/java/zas/admin/zia/translation/ZiaTranslationApplication.java
@@ -0,0 +1,16 @@
+package zas.admin.zia.translation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiaTranslationApplication {
+    private ZiaTranslationApplication() {
+        /* This utility class should not be instantiated */
+    }
+
+    static void main(String[] args) {
+        SpringApplication.run(ZiaTranslationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zas/admin/zia/translation/ZiaTranslationApplicationTests.java
@@ -0,0 +1,13 @@
+package zas.admin.zia.translation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiaTranslationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
